5ac887cb24fb1f5543813e59f10d6bf8aa484e88
Agung <agungskripsi05@gmail.com>
2022-09-26 11:03:01 +0700

    ssad

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,7 @@ Route::get('/admin/pelanggan', function () {
 });
 
 Route::get('/test', function () {
-    return 'hello 12';
+    return 'hello 123';
 });
 
 Route::get('/bootstrap', function () {
