351bc647c2e96902d34ebdc31fc5d9975f947f86
Agung <agungskripsi05@gmail.com>
2022-09-28 09:42:57 +0700

    example test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -13,6 +13,7 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
-        $this->assertTrue(true);
+        $output = 9;
+        $this->assertEquals(9, $output);
     }
 }
